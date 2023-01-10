28ca34676c06ec2bd43126e8de9d185cadd09653
jinnyjinnyjinjin <kkam2ya@gmail.com>
2023-01-10 21:08:24 +0900

    first commit

---
+++ src/main/java/com/jinnyjinnyjinjin/pharmacyrecommend/PharmacyRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.jinnyjinnyjinjin.pharmacyrecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyRecommendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyRecommendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jinnyjinnyjinjin/pharmacyrecommend/PharmacyRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinnyjinnyjinjin.pharmacyrecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyRecommendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
