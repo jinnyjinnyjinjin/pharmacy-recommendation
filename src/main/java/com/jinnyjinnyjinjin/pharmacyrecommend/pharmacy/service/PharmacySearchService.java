a733064dfdc77a9e746e8124bdf6b03149eb07fc
jinnyjinnyjinjin <kkam2ya@gmail.com>
2023-01-14 16:49:01 +0900

    direction.sql 추가

---
+++ src/main/java/com/jinnyjinnyjinjin/pharmacyrecommend/pharmacy/service/PharmacySearchService.java
@@ -0,0 +1,32 @@
+package com.jinnyjinnyjinjin.pharmacyrecommend.pharmacy.service;
+
+import com.jinnyjinnyjinjin.pharmacyrecommend.pharmacy.dto.PharmacyDto;
+import com.jinnyjinnyjinjin.pharmacyrecommend.pharmacy.entity.Pharmacy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PharmacySearchService {
+
+    private final PharmacyRepositoryService pharmacyRepositoryService;
+
+    public List<PharmacyDto> searchPharmacyDtoList() {
+        return pharmacyRepositoryService.findAll().stream()
+                .map(this::convertToPharmacyDto)
+                .collect(Collectors.toList());
+    }
+
+    private PharmacyDto convertToPharmacyDto(Pharmacy pharmacy) {
+        return PharmacyDto.builder()
+                .id(pharmacy.getId())
+                .pharmacyAddress(pharmacy.getPharmacyAddress())
+                .pharmacyName(pharmacy.getPharmacyName())
+                .latitude(pharmacy.getLatitude())
+                .longitude(pharmacy.getLongitude())
+                .build();
+    }
+}
