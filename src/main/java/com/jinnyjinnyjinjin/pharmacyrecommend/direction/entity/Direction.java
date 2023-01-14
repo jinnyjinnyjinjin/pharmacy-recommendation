21eee4cc0ac689eecfa0f1103ba1bf8200d3b8a0
jinnyjinnyjinjin <kkam2ya@gmail.com>
2023-01-14 16:47:13 +0900

    Direction Entity 추가

---
+++ src/main/java/com/jinnyjinnyjinjin/pharmacyrecommend/direction/entity/Direction.java
@@ -0,0 +1,34 @@
+package com.jinnyjinnyjinjin.pharmacyrecommend.direction.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Direction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String inputAddress;
+    private double inputLatitude;
+    private double inputLongitude;
+
+    private String targetPharmacyName;
+    private String targetAddress;
+    private double targetLatitude;
+    private double targetLongitude;
+
+    private double distance;
+}
