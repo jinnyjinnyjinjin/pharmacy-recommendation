7ddea1862624dfac3c01a80a0667b925d537c3f7
jinnyjinnyjinjin <kkam2ya@gmail.com>
2023-01-14 17:55:52 +0900

    DirectionRepository 추가

---
+++ src/main/java/com/jinnyjinnyjinjin/pharmacyrecommend/direction/repository/DirectionRepository.java
@@ -0,0 +1,7 @@
+package com.jinnyjinnyjinjin.pharmacyrecommend.direction.repository;
+
+import com.jinnyjinnyjinjin.pharmacyrecommend.direction.entity.Direction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DirectionRepository extends JpaRepository<Direction, Long> {
+}
