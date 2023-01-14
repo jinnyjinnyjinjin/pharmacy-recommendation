4beb1161172717d8c5f1d1fa56a5ef6b1d393dfb
jinnyjinnyjinjin <kkam2ya@gmail.com>
2023-01-14 15:52:25 +0900

    Pharmacy Entity 추가

---
+++ src/main/java/com/jinnyjinnyjinjin/pharmacyrecommend/pharmacy/entity/Pharmacy.java
@@ -0,0 +1,32 @@
+package com.jinnyjinnyjinjin.pharmacyrecommend.pharmacy.entity;
+
+import com.jinnyjinnyjinjin.pharmacyrecommend.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pharmacy extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String pharmacyName;
+    private String pharmacyAddress;
+    private double latitude;
+    private double longitude;
+
+    public void changePharmacyAddress(String address) {
+        this.pharmacyAddress = address;
+    }
+}
